a1e207b2ceb8176a6a36b20f40b90c4d2be3ec40
hankdoo <hungdv21082002@gmail.com>
2023-12-17 18:07:47 +0700

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,47 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+<script >
+export default {
+  data() {
+    return {
+      author: {
+        name: 'John Doe',
+        books: [
+          'Vue 2 - Advanced Guide',
+          'Vue 3 - Basic Guide',
+          'Vue 4 - The Mystery'
+        ]
+      }
+    }
+  },
+
+  computed: {
+    // a computed getter
+    publishedBooksMessage() {
+      // `this` points to the component instance
+      return this.author.books.length > 0 ? 'Yes' : 'No'
+    },
+    now() {
+      return Date.now()
+    }
+  },
+  methods: {
+    calculateBooksMessage() {
+      return this.author.books.length > 0 ? 'Yes' : 'No'
+    }
+  }
+
+}
+
+
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
+  <div>
+    <div>{{ author.name }}</div>
+    <span>{{ author.books.length > 0 ? 'Yes' : 'No' }}</span>
+    <span>{{ publishedBooksMessage }}</span>
+    <p>{{ calculateBooksMessage() }}</p>
+    <p>{{ now }}</p>
   </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
+<style></style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,56 @@
+<script >
+export default {
+  data() {
+    return {
+      firstName: 'hung',
+      lastName: 'Do'
+    }
+  },
+  computed: {
+    fullName: {
+      // getter
+      get() {
+        return this.firstName + ' ' + this.lastName
+      },
+      // setter
+      set(newValue) {
+        // Note: we are using destructuring assignment syntax here.
+        [this.firstName, this.lastName] = newValue.split(' ')
+      }
+    }
+  },
+  methods: {
+    say(message) {
+      alert(message)
+    },
+    warn(message, event) {
+      // now we have access to the native event
+      if (event) {
+        event.preventDefault()
+      }
+      alert(message)
+    }
+  }
+}
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div>helllo</div>
+    <p>{{ fullName.get }}</p>
+    <button v-on:click="fullName.set"></button>
+    <div>helllo</div>
+    <button @click="say('hello')">Say hello</button>
+    <button @click="say('bye')">Say bye</button>
+    <button @click="warn('Form cannot be submitted yet.', $event)">
+      Submit
+    </button>
+
+    <!-- using inline arrow function -->
+    <button @click="(event) => warn('Form cannot be submitted yet.', event)">
+      Submit
+    </button>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style></style>
